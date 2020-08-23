90ec933d225ff15087d8d9f5921b18a94b7949c2
vudinhquang <vudinhquangk53@gmail.com>
2020-08-23 21:06:04 +0900

    Email & Date Fields

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -12,7 +12,7 @@ class ContactsController extends Controller
 
         $data = request()->validate([
             'name' => 'required',
-            'email' => 'required',
+            'email' => 'required|email',
             'birthday' => 'required',
             'company' => 'required',
         ]);
--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 use App\Contact;
+use DateTime;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -36,6 +38,28 @@ class ContactsTest extends TestCase
             });
     }
 
+    /** @test */
+    public function email_must_be_a_valid_email()
+    {
+        $response = $this->post('/api/contacts',
+            array_merge($this->data(), ['email' => 'NOT AN EMAIL']));
+
+        $response->assertSessionHasErrors('email');
+        $this->assertCount(0, Contact::all());
+    }
+
+    /** @test */
+    public function birthdays_are_properly_stored()
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this->post('/api/contacts',
+            array_merge($this->data()));
+
+        $this->assertCount(1, Contact::all());
+        $this->assertEquals('05-14-1988', $this->parseCarbon(Contact::first()->birthday));
+    }
+
     private function data()
     {
         return [
@@ -44,5 +68,9 @@ class ContactsTest extends TestCase
             'birthday' => '05/14/1988',
             'company' => 'ABC String'
         ];
+    }
+
+    private function parseCarbon($date) {
+        return Carbon::parse(Contact::first()->birthday)->format('m-d-Y');
     }
 }
