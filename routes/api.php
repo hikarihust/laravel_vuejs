40d1701b06cbe1db2e58dfe0ea2daf671c061d9f
vudinhquang <vudinhquangk53@gmail.com>
2020-08-25 00:40:08 +0900

    Contacts Belong to Users

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->group(function () {
-    Route::get('/contacts/{contact}', 'ContactsController@show');
+    Route::get('/contacts', 'ContactsController@index');
     Route::post('/contacts', 'ContactsController@store');
+    Route::get('/contacts/{contact}', 'ContactsController@show');
     Route::patch('/contacts/{contact}', 'ContactsController@update');
     Route::delete('/contacts/{contact}', 'ContactsController@destroy');
 });
