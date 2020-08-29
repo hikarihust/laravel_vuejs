ef968b8c0b03094a6578e29c45f853362b272142
vudinhquang <vudinhquangk53@gmail.com>
2020-08-30 01:37:10 +0900

    Fetching a Single Contact

---
+++ resources/js/views/ContactsShow.vue
@@ -0,0 +1,31 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "ContactsShow",
+
+        mounted() {
+            axios.get('/api/contacts/' + this.$route.params.id)
+                .then(response => {
+                    this.contact = response.data.data;
+                })
+                .catch(error => {
+                    if (error.response.status === 404) {
+                        this.$router.push('/contacts');
+                    }
+                });
+        },
+
+        data: function () {
+            return {
+                contact: null,
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
