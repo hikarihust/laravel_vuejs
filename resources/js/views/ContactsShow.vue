6a5a7035f1e87351815407c223b253c32676c882
vudinhquang <vudinhquangk53@gmail.com>
2020-08-30 09:24:18 +0900

    Redirecting After Create

--- resources/js/views/ContactsShow.vue
+++ resources/js/views/ContactsShow.vue
@@ -1,29 +1,32 @@
 <template>
-    <div v-if="contact">
-        <div class="flex justify-between">
-            <a href="#" class="text-blue-400">
-                < Back
-            </a>
-            <div>
-                <router-link :to="'/contacts/' + contact.contact_id + '/edit'" class="px-4 py-2 rounded text-green-500
-                    border border-green-500 text-sm font-bold mr-2">Edit</router-link>
-                <a href="#" class="px-4 py-2 border border-red-500 rounded text-sm font-bold text-red-500">Delete</a>
+    <div>
+        <div v-if="loading">Loading...</div>
+        <div v-else>
+            <div class="flex justify-between">
+                <a href="#" class="text-blue-400" @click="$router.back()">
+                    < Back
+                </a>
+                <div>
+                    <router-link :to="'/contacts/' + contact.contact_id + '/edit'" class="px-4 py-2 rounded text-green-500
+                        border border-green-500 text-sm font-bold mr-2">Edit</router-link>
+                    <a href="#" class="px-4 py-2 border border-red-500 rounded text-sm font-bold text-red-500">Delete</a>
+                </div>
             </div>
+
+            <div class="flex items-center pt-6">
+                <UserCircle :name="contact.name" />
+                <p class="pl-5 text-xl">{{ contact.name }}</p>
+            </div>
+
+            <p class="pt-6 text-gray-600 font-bold uppercase text-sm">Contact</p>
+            <p class="pt-2 text-blue-400">{{ contact.email }}</p>
+
+            <p class="pt-6 text-gray-600 font-bold uppercase text-sm">Company</p>
+            <p class="pt-2 text-blue-400">{{ contact.company }}</p>
+
+            <p class="pt-6 text-gray-600 font-bold uppercase text-sm">Birthday</p>
+            <p class="pt-2 text-blue-400">{{ contact.birthday }}</p>
         </div>
-
-        <div class="flex items-center pt-6">
-            <UserCircle :name="contact.name" />
-            <p class="pl-5 text-xl">{{ contact.name }}</p>
-        </div>
-
-        <p class="pt-6 text-gray-600 font-bold uppercase text-sm">Contact</p>
-        <p class="pt-2 text-blue-400">{{ contact.email }}</p>
-
-        <p class="pt-6 text-gray-600 font-bold uppercase text-sm">Company</p>
-        <p class="pt-2 text-blue-400">{{ contact.company }}</p>
-
-        <p class="pt-6 text-gray-600 font-bold uppercase text-sm">Birthday</p>
-        <p class="pt-2 text-blue-400">{{ contact.birthday }}</p>
     </div>
 </template>
 
@@ -41,8 +44,10 @@
             axios.get('/api/contacts/' + this.$route.params.id)
                 .then(response => {
                     this.contact = response.data.data;
+                    this.loading = false;
                 })
                 .catch(error => {
+                    this.loading = false;
                     if (error.response.status === 404) {
                         this.$router.push('/contacts');
                     }
@@ -51,6 +56,7 @@
 
         data: function () {
             return {
+                loading: true,
                 contact: null,
             }
         },
