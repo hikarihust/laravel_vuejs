6031ff4b9466c5f1d7d4d90bb5cf2590d8d7c8e4
vudinhquang <vudinhquangk53@gmail.com>
2020-08-30 22:55:24 +0900

    Birthday view

--- resources/js/router.js
+++ resources/js/router.js
@@ -5,6 +5,7 @@ import ContactsCreate from "./views/ContactsCreate";
 import ContactsShow from "./views/ContactsShow";
 import ContactsEdit from "./views/ContactsEdit";
 import ContactsIndex from "./views/ContactsIndex";
+import BirthdaysIndex from "./views/BirthdaysIndex";
 
 Vue.use(VueRouter);
 
@@ -25,6 +26,10 @@ export default new VueRouter({
         },
         {
             path: '/contacts/:id/edit', component: ContactsEdit,
+        },
+
+        {
+            path: '/birthdays', component: BirthdaysIndex,
         }
     ],
     mode: 'history'
