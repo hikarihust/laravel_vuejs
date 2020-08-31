61f1cb1ee72e3d91323474b90664e25baf0d3477
vudinhquang <vudinhquangk53@gmail.com>
2020-09-01 00:45:21 +0900

    Document Titles

--- resources/js/router.js
+++ resources/js/router.js
@@ -17,19 +17,24 @@ export default new VueRouter({
         },
         {
             path: '/contacts', component: ContactsIndex,
+            meta: { title: 'Contacts' }
         },
         {
-            path: '/contacts/create', component: ContactsCreate
+            path: '/contacts/create', component: ContactsCreate,
+            meta: { title: 'Add New Contact' }
         },
         {
             path: '/contacts/:id', component: ContactsShow,
+            meta: { title: 'Details for Contact' }
         },
         {
             path: '/contacts/:id/edit', component: ContactsEdit,
+            meta: { title: 'Edit Contact' }
         },
 
         {
             path: '/birthdays', component: BirthdaysIndex,
+            meta: { title: 'This Month\'s Birthdays' }
         }
     ],
     mode: 'history'
